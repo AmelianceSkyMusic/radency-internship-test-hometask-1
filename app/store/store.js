aabab3d0975239995e3fd3168a3a7e0e88abc83c
AmelianceSkyMusic <AmelianceSkyMusic@gmail.com>
2023-08-02 15:54:45 +0300

    refactor: add button functions to store - archive - remove

--- app/store/store.js
+++ app/store/store.js
@@ -148,4 +148,42 @@ export const store = observable({
 	notesList,
 	archivedNotesList,
 	activeTab: 'notes',
+
+	moveNoteToArchive: (id) => {
+		const newArchivedNotesList = [...store.data.archivedNotesList];
+		const newNotesList = store.data.notesList.filter((note) => {
+			if (note.id !== id) return true;
+			newArchivedNotesList.push(note);
+		});
+		store.update({ ...store.data, notesList: newNotesList, archivedNotesList: newArchivedNotesList });
+	},
+	removeNoteFromNotesList: (id) => {
+		const newNotesList = store.data.notesList.filter((note) => note.id !== id);
+		store.update({ ...store.data, notesList: newNotesList });
+	},
+	moveAllNoteToArchive: () => {
+		store.update({ ...store.data, notesList: [], archivedNotesList: [...archivedNotesList, ...notesList] });
+	},
+	removeAllNotesList: () => {
+		store.update({ ...store.data, notesList: [] });
+	},
+
+	moveNoteFromArchive: (id) => {
+		const newNotesList = [...store.data.notesList];
+		const newArchivedNotesList = store.data.archivedNotesList.filter((note) => {
+			if (note.id !== id) return true;
+			newNotesList.push(note);
+		});
+		store.update({ ...store.data, archivedNotesList: newArchivedNotesList, notesList: newNotesList });
+	},
+	removeNoteFromArchivedList: (id) => {
+		const newArchivedNotesList = store.data.archivedNotesList.filter((note) => note.id !== id);
+		store.update({ ...store.data, archivedNotesList: newArchivedNotesList });
+	},
+	moveAllNoteFromArchive: () => {
+		store.update({ ...store.data, notesList: [...notesList, ...archivedNotesList], archivedNotesList: [] });
+	},
+	removeAllArchivedNotesList: () => {
+		store.update({ ...store.data, archivedNotesList: [] });
+	},
 });
