b9fddebdb82a9a638aa31aa9bb9ba40cdb74e461
AmelianceSkyMusic <AmelianceSkyMusic@gmail.com>
2023-08-02 16:07:56 +0300

    refactor: update createTable implementation

--- app/tables/createTables.js
+++ app/tables/createTables.js
@@ -2,7 +2,6 @@ import { createNoteTable } from './noteTable/createNoteTable.js';
 import { createSummaryTable } from './summaryTable/createSummaryTable.js';
 
 export function createTables(data) {
-	if (data.activeTab === 'notes') createNoteTable(data.notesList, data.activeTab);
-	if (data.activeTab !== 'notes') createNoteTable(data.archivedNotesList, data.activeTab);
+	createNoteTable(data);
 	createSummaryTable(data);
 }
