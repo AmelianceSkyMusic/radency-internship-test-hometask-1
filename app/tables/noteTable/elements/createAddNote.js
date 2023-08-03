db12b89f916750b6987e1006b743623428fd3f40
AmelianceSkyMusic <AmelianceSkyMusic@gmail.com>
2023-08-03 11:08:29 +0300

    refactor: move createAddNote to elements

--- app/tables/noteTable/elements/createAddNote.js
+++ app/tables/noteTable/elements/createAddNote.js
@@ -1,11 +1,11 @@
-import { createHTMLElem } from '../../../../helpers/createHTMLElem.js';
+import { createHTMLElem } from '../../../helpers/createHTMLElem.js';
+import { addNote } from '../addNote.js';
 
 export function createAddNote(parent$) {
 	const addNote$ = createHTMLElem(parent$, 'div', { class: 'add-note' });
 
 	const addNoteButton$ = createHTMLElem(addNote$, 'button', { class: 'add-note__button' }, '➕ Add note');
-	addNoteButton$.addEventListener('click', () => {
-	});
+	addNoteButton$.addEventListener('click', addNote);
 
 	return parent$;
 }
