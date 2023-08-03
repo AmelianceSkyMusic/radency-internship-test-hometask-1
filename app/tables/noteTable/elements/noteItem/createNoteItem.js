b531fce64c937bac80e7a718867e0e7dc00d3571
AmelianceSkyMusic <AmelianceSkyMusic@gmail.com>
2023-08-03 10:50:27 +0300

    refactor: add icon column in notes table

--- app/tables/noteTable/elements/noteItem/createNoteItem.js
+++ app/tables/noteTable/elements/noteItem/createNoteItem.js
@@ -1,12 +1,14 @@
 import { createHTMLElem } from '../../../../helpers/createHTMLElem.js';
 
-export function createNoteItem(parent$, createNoteItemButtons, note, data) {
+export function createNoteItem(parent$, createNoteItemButtons, id, icon, name, created, category, content, dates) {
 	const noteItem$ = createHTMLElem(parent$, 'div', { class: 'note-item' });
 
-	createHTMLElem(noteItem$, 'p', { class: 'note-item__created' }, note.created);
-	createHTMLElem(noteItem$, 'p', { class: 'note-item__category' }, note.category);
-	createHTMLElem(noteItem$, 'p', { class: 'note-item__content' }, note.content);
-	createHTMLElem(noteItem$, 'p', { class: 'note-item__dates' }, note.dates);
+	createHTMLElem(noteItem$, 'p', { class: 'note-item-cell note-item__icon' }, icon);
+	createHTMLElem(noteItem$, 'p', { class: 'note-item-cell note-item__name' }, name);
+	createHTMLElem(noteItem$, 'p', { class: 'note-item-cell note-item__created' }, created);
+	createHTMLElem(noteItem$, 'p', { class: 'note-item-cell note-item__category' }, category);
+	createHTMLElem(noteItem$, 'p', { class: 'note-item-cell note-item__content' }, content);
+	createHTMLElem(noteItem$, 'p', { class: 'note-item-cell note-item__dates' }, dates);
 
 	const noteItemButtons$ = createHTMLElem(noteItem$, 'div', { class: 'note-item__buttons note-item-buttons' });
 	createNoteItemButtons(noteItemButtons$, note.id);
