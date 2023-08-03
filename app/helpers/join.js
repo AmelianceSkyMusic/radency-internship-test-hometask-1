e835a67587891f0bbde07ed9b5b32e330e85a194
AmelianceSkyMusic <AmelianceSkyMusic@gmail.com>
2023-08-03 10:47:16 +0300

    refactor: add trycatch to join helper

--- app/helpers/join.js
+++ app/helpers/join.js
@@ -1,22 +1,32 @@
+import { returnError } from './returnError.js';
+
 function flatObjectKeysToArray(obj) {
-	const entries = Object.entries(obj);
-	if (entries.length > 0) {
-		return entries.map((tuple) => (tuple[1] ? tuple[0] : '')).filter((element) => element);
+	try {
+		const entries = Object.entries(obj);
+		if (entries.length > 0) {
+			return entries.map((tuple) => (tuple[1] ? tuple[0] : '')).filter((element) => element);
+		}
+		return [];
+	} catch (error) {
+		returnError(error);
 	}
-	return [];
 }
 
 export function join(...args) {
-	return args
-		.flat(Infinity)
-		.map((element) => {
-			if (element && typeof element === 'object') {
-				const flatObjectKeysArray = flatObjectKeysToArray(element);
-				return flatObjectKeysArray ? flatObjectKeysArray.join(' ') : '';
-			}
-			return element;
-		})
-		.filter((element) => element)
-		.join(' ')
-		.trim();
+	try {
+		return args
+			.flat(Infinity)
+			.map((element) => {
+				if (element && typeof element === 'object') {
+					const flatObjectKeysArray = flatObjectKeysToArray(element);
+					return flatObjectKeysArray ? flatObjectKeysArray.join(' ') : '';
+				}
+				return element;
+			})
+			.filter((element) => element)
+			.join(' ')
+			.trim();
+	} catch (error) {
+		returnError(error);
+	}
 }
