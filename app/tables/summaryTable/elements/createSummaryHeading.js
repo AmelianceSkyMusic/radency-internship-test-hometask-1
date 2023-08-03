53b4705e7e6090a48af4dac3fae795131fa20803
AmelianceSkyMusic <AmelianceSkyMusic@gmail.com>
2023-08-03 10:50:42 +0300

    refactor: add icon column in summary table

--- app/tables/summaryTable/elements/createSummaryHeading.js
+++ app/tables/summaryTable/elements/createSummaryHeading.js
@@ -3,9 +3,10 @@ import { createHTMLElem } from '../../../helpers/createHTMLElem.js';
 export function createSummaryHeading(parent$, categoryTitle, activeTitle, archivedTitle) {
 	const summaryHeading$ = createHTMLElem(parent$, 'div', { class: 'summary-heading' });
 
-	createHTMLElem(summaryHeading$, 'h5', { class: 'summary-heading__category' }, categoryTitle);
-	createHTMLElem(summaryHeading$, 'h5', { class: 'summary-heading__active' }, activeTitle);
-	createHTMLElem(summaryHeading$, 'h5', { class: 'summary-heading__archived' }, archivedTitle);
+	createHTMLElem(summaryHeading$, 'h5', { class: 'summary-heading-cell summary-heading__icon' });
+	createHTMLElem(summaryHeading$, 'h5', { class: 'summary-heading-cell summary-heading__category' }, categoryTitle);
+	createHTMLElem(summaryHeading$, 'h5', { class: 'summary-heading-cell summary-heading__active' }, activeTitle);
+	createHTMLElem(summaryHeading$, 'h5', { class: 'summary-heading-cell summary-heading__archived' }, archivedTitle);
 
 	return parent$;
 }
--- app/tables/summaryTable/elements/createSummaryItem.js
+++ app/tables/summaryTable/elements/createSummaryItem.js
@@ -1,11 +1,12 @@
 import { createHTMLElem } from '../../../helpers/createHTMLElem.js';
 
-export function createSummaryItem(parent$, category, active, archived) {
+export function createSummaryItem(parent$, icon, category, active, archived) {
 	const summaryItem$ = createHTMLElem(parent$, 'div', { class: 'summary-item' });
 
-	createHTMLElem(summaryItem$, 'p', { class: 'summary-item__category' }, category);
-	createHTMLElem(summaryItem$, 'p', { class: 'summary-item__active' }, active);
-	createHTMLElem(summaryItem$, 'p', { class: 'summary-item__archived' }, archived);
+	createHTMLElem(summaryItem$, 'p', { class: 'summary-item-cell summary-item__icon' }, icon);
+	createHTMLElem(summaryItem$, 'p', { class: 'summary-item-cell summary-item__category' }, category);
+	createHTMLElem(summaryItem$, 'p', { class: 'summary-item-cell summary-item__active' }, active);
+	createHTMLElem(summaryItem$, 'p', { class: 'summary-item-cell summary-item__archived' }, archived);
 
 	return parent$;
 }
