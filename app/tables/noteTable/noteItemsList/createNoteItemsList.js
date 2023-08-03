211766bc1964494fc090f8bb1b4fa362edd4805c
AmelianceSkyMusic <AmelianceSkyMusic@gmail.com>
2023-08-03 11:13:42 +0300

    refactor: update create notes with data

--- app/tables/noteTable/noteItemsList/createNoteItemsList.js
+++ app/tables/noteTable/noteItemsList/createNoteItemsList.js
@@ -2,13 +2,24 @@ import { createArchivedNoteItemButtons } from '../elements/noteItem/createArchiv
 import { createHTMLElem } from '../../../helpers/createHTMLElem.js';
 import { createNoteItem } from '../elements/noteItem/createNoteItem.js';
 import { createNoteItemButtons } from '../elements/noteItem/createNoteItemButtons.js';
+import { CATEGORY_ICON } from '../../../constants/CATEGORY_ICON.js';
+import { CATEGORIES } from '../../../constants/CATEGORIES.js';
+import { getFormattedDate } from '../../../helpers/getFormattedDate.js';
+import { parseDateFromString } from '../../../helpers/parseDateFromString.js';
+import { sortArrayOfObj } from '../../../helpers/sortArrayOfObj.js';
 
 export function createNoteItemsList(parent$, list, data) {
 	const noteList$ = createHTMLElem(parent$, 'div', { class: 'note-items-list' });
 
 	const buttonCreateCallback = data.activeTab === 'notes' ? createNoteItemButtons : createArchivedNoteItemButtons;
-	list.forEach((note) => {
-		createNoteItem(noteList$, buttonCreateCallback, note, data);
+	const sortedList = sortArrayOfObj(list, 'created');
+	sortedList.sort().forEach((note) => {
+		const icon = CATEGORY_ICON[note.category];
+		const category = CATEGORIES[note.category];
+		const { id, name, created: createdMs, content } = note;
+		const created = getFormattedDate(createdMs);
+		const dates = parseDateFromString(content)?.join(', ');
+		createNoteItem(noteList$, buttonCreateCallback, id, icon, name, created, category, content, dates);
 	});
 
 	return noteList$;
